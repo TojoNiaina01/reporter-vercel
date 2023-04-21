585f6b526562507f5da354eb6281c542fe8d70e6
Tojoniaina01 <Tojokael01@gmail.com>
2023-04-21 09:38:49 +0300

    save

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
 import Layout from "@/Layout/Layout";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+export default function App({ Component, pageProps, router }) {
+  const getLayout = () => {
+    if (router.pathname.startsWith("/admin")) {
+      return <Component {...pageProps} />;
+    }
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  };
+  return getLayout();
 }
---
+++ pages/admin/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Admin = () => {
+  return (
+    <div>
+      <div>
+        <h3>Welcome!</h3>
+        <h2>Admin Name</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
