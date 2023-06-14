6160cd1b2a62df0945de0b09e639e99b37178ead
Mandreshi <andriatsimihambomanana@gmail.com>
2023-06-14 16:34:14 +0300

    modif next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 // const withImages = require('next-images')
 
-
-// module.exports = withImages({
-//   //reactStrictMode: true,
-//   webpack(config, options) {
-//     return config
-//   }
-// })
-
-
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
@@ -32,20 +23,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// const withImages = require('next-images');
-// const ImageminPlugin = require('imagemin-webpack-plugin').default;
-
-// module.exports = withImages({
-//   webpack(config, { isServer }) {
-//     if (!isServer) {
-//       config.plugins.push(
-//         new ImageminPlugin({
-//           test: /\.(jpe?g|png|gif|svg)$/i,
-//         })
-//       );
-//     }
-
-//     return config;
-//   },
-// });
